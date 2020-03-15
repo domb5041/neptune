0c7c33b2b197c4b680da92cd6934bcabeb96a342
Dominic Butler 2 <dominicbutler2@Dominics-MacBook-Pro.local>
2020-03-15 15:23:26 +0000

    router setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Route, Switch, Link} from 'react-router-dom'
+import Home from './pages/Home'
+import Page1 from './pages/Page1'
+import Page2 from './pages/Page2'
+import Page3 from './pages/Page3'
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className='nav-bar'>
+          <Link to='/'>Home</Link>
+          <Link to='/page1'>Page 1</Link>
+          <Link to='/page2'>Page 2</Link>
+          <Link to='/page3'>Page 3</Link>
+        </nav>
       </header>
+      <main>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route path='/page1' component={Page1}/>
+          <Route path='/page2' component={Page2}/>
+          <Route path='/page3' component={Page3}/>
+        </Switch>
+      </main>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="page">
+      home
+    </div>
+  );
+}
+
+export default Home;
